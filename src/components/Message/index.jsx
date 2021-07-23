1dae063c39e6317f367c021c035d17af614dcaa1
Tatiane <weitzeltati@gmail.com>
2021-07-22 21:05:09 -0300

    first commit

---
+++ src/components/Message/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+const Messages = () => {
+  return (
+    <div className="messagesText">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        perspiciatis. Libero tenetur quia nam recusandae quidem fugiat eligendi
+        consectetur voluptatem blanditiis fugit at nulla quo, magnam, corporis
+        nostrum itaque praesentium!
+      </p>
+    </div>
+  );
+};
+
+export default Messages;
---
+++ src/components/MessageUser2/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+const MessageUser2 = () => {
+  return (
+    <article className="messageTextUser2">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+        perspiciatis. Libero tenetur quia nam recusandae quidem fugiat eligendi
+      </p>
+    </article>
+  );
+};
+
+export default MessageUser2;
